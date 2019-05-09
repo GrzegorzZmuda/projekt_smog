d6c5de8b894705225d6de3e67eceeff81ad9d867
Weronika Niedzwidz <wmniedzwiedz@gmail.com>
2019-05-09 19:18:47 +0200

    Usunięcie niepotrzebnych danych, poprawione czytanie zanieczyszczenia dla stacji

--- stacje.py
+++ stacje.py
@@ -16,6 +16,7 @@ stacje = s.sheet_by_index(0)
 
 p = xlrd.open_workbook('dane/2017_PM10_1g.xlsx')
 pm10 = p.sheet_by_index(0)
+pc=pm10.ncols
 
 w, h = 4, 12;
 res = [[0 for x in range(w)] for y in range(h)] #w tym jest zapisane id i koordynaty
@@ -25,9 +26,12 @@ for i in range (12):
     res[i][0]=stacje.cell(i, 1).value
     res[i][1]=stacje.cell(i, 14).value
     res[i][2]=stacje.cell(i, 15).value
-    if (pm10.cell(6,i+37).value != ''):
-        q=pm10.cell(6,i+37).value
-        res[i][3]=float(q.replace(',', '.'))
     
+    for j in range (pc):
     
-    plt.scatter(res[i][1],res[i][2],s=res[i][3]+1)
+        if (pm10.cell(1,j).value == res[i][0] and pm10.cell(6,j).value!=""):
+            q=pm10.cell(6,j).value
+            res[i][3]=float(q.replace(',', '.'))
+        
+    
+    plt.scatter(res[i][1],res[i][2],s=res[i][3]+10)
