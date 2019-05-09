476d1871d35ea3d7e147eb88a07182596e21966c
Weronika Niedzwidz <wmniedzwiedz@gmail.com>
2019-05-09 19:20:11 +0200

    Revert "Merge branch 'master' of https://github.com/GrzegorzZmuda/projekt_smog"
    This reverts commit 330cbbafd43fefa9f539883c90f9b9cc9d1acb0e, reversing
    changes made to d6c5de8b894705225d6de3e67eceeff81ad9d867.

--- ModelB.py
+++ ModelB.py
@@ -20,21 +20,14 @@ Model teoretycznie działa ale trzeba go ładnie wyswietlić, zrobię to jutro
 
 
 
-import time
-import numpy as np
-import os
 
-import pygame
-from pygame import surfarray
-from pygame.locals import *
-from numpy import int32
-pygame.init()
+import numpy as np
+#import pygame
+
+#pygame.init()
 from chart import chart
 
-
 gridsize=50
-scale=8
-
 
 dff=0.05 #ile zanieczyszczeń przechodzi na sąsiadującą kratkę
 sd=0.99  #ile zanieczyszczeń pozostaje w cyklu
@@ -44,51 +37,13 @@ sources=np.zeros((gridsize,gridsize))
 sources[25][25]=100
 
 
-def convert(temp):
-    temp=temp/10
-    if (temp>500):
-        red= 1000-temp
-    else:
-        red=0
-    green=abs(temp-500)
-    
-    if temp<500:
-        blue=temp
-    else:
-        blue=0
-    return (red*255/500,green*255/500,blue*255/500)
 
-def show (data) :
-    
-    screen = pygame.display.set_mode((data.shape[0]*scale,data.shape[1]*scale), 0, 32)
-    img= np.zeros((data.shape[0],data.shape[1],3))
-    for x in range(data.shape[0]):
-        for y in range(data.shape[1]):
-                img[x,y,:]=convert(data[x,y])
-   
-    
-    
-    surfarray.blit_array(screen, img.repeat(scale,axis=0).repeat(scale,axis=1))
-   
-    pygame.display.flip()
-    
-    pygame.display.set_caption("test")
-    """
-    while 1:
-        e = pygame.event.wait()
-        if e.type == MOUSEBUTTONDOWN: break
-        elif e.type == QUIT:
-            pygame.quit()
-            raise SystemExit()
-    """
-    
-    time.sleep(0.1)
 
 grid=np.zeros((gridsize,gridsize))
 
 
 
-for abdas in range(200):
+for abdas in range(100):
     gridn=np.zeros((gridsize,gridsize))
 
     
@@ -105,11 +60,8 @@ for abdas in range(200):
     
     for x in range(1,gridsize-1):
         for y in range(1,gridsize-1): # sorces + remaining pollution from previous cycle (spreaded pollution)
-            gridn[x][y]=gridn[x][y]+sd*(dff*(grid[x-1][y]+grid[x+1][y]+grid[x][y-1]+grid[x][y+1])+(1-dff)*grid[x][y])
+            gridn[x-1][y]=gridn[x][y]+sd*(dff*(grid[x-1][y]+grid[x+1][y]+grid[x][y-1]+grid[x][y+1])+(1-dff)*grid[x][y])
 
-    #chart(grid, gridsize, gridsize)
+    chart(grid, gridsize, gridsize)
     grid=gridn
 
-    show(grid)
-
-pygame.quit()
