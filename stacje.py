a4e5e91dba648bfbf0de4f9b94f9c310c184fa84
Weronika Niedzwidz <wmniedzwiedz@gmail.com>
2019-05-09 04:02:33 +0200

    Czyta jeszcze kawałek danych pomiarowych

--- stacje.py
+++ stacje.py
@@ -2,7 +2,7 @@
 """
 Created on Thu May  9 03:34:40 2019
 
-czyta położenie stacji + podgląd ich rozkładu
+czyta położenie stacji + podgląd ich rozkładu + rozmiar zależny od poziomu PM10 dnia 01.01.2017
 
 @author: pi
 """
@@ -14,7 +14,10 @@ import matplotlib.pyplot as plt
 s = xlrd.open_workbook('dane/MetadaneKrk.xls')
 stacje = s.sheet_by_index(0)
 
-w, h = 3, 12;
+p = xlrd.open_workbook('dane/2017_PM10_1g.xlsx')
+pm10 = p.sheet_by_index(0)
+
+w, h = 4, 12;
 res = [[0 for x in range(w)] for y in range(h)] #w tym jest zapisane id i koordynaty
 
 for i in range (12):
@@ -22,6 +25,9 @@ for i in range (12):
     res[i][0]=stacje.cell(i, 1).value
     res[i][1]=stacje.cell(i, 14).value
     res[i][2]=stacje.cell(i, 15).value
+    if (pm10.cell(6,i+37).value != ''):
+        q=pm10.cell(6,i+37).value
+        res[i][3]=float(q.replace(',', '.'))
     
     
-    plt.scatter(res[i][1],res[i][2])
+    plt.scatter(res[i][1],res[i][2],s=res[i][3]+1)
