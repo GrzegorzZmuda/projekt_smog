260377654bb6a7b4921181f607b66e517dbd32a7
Lordbaysel <lordbaysel@gmail.com>
2019-06-02 23:02:35 +0200

    redduce request spam

--- PolutionAPIchache.py
+++ PolutionAPIchache.py
@@ -1,12 +1,40 @@
 import json
+import time
+
 import PolutionAPI
 
 
 def airdata():
-    a=PolutionAPI.getSensors()
-    with open('air.json', 'w') as json_file:
-        json.dump(a, json_file)
+    tmp=time.time()
+
+    with open('air.json','r') as json_file:
+        a = json.load(json_file)
+
+
+    if a[-1]-tmp>1800:
+        a = PolutionAPI.getSensors()
+        a.append(tmp)
+        with open('air.json', 'w') as json_file:
+            json.dump(a, json_file)
 
     with open('air.json','r') as json_file:
         a = json.load(json_file)
     return(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
