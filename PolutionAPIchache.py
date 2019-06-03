bece4e1427138227a573c15c6d087a9a0c94f781
GrzegorzZmuda <33518770+GrzegorzZmuda@users.noreply.github.com>
2019-06-03 16:55:20 +0200

    Update PolutionAPIchache.py

--- PolutionAPIchache.py
+++ PolutionAPIchache.py
@@ -7,19 +7,22 @@ import PolutionAPI
 def airdata():
     tmp=time.time()
 
+
     with open('air.json','r') as json_file:
-        a = json.load(json_file)
+        b = json.load(json_file)
 
 
-    if a[-1]-tmp>1800:
-        a = PolutionAPI.getSensors()
+    if tmp-b[-1]>1799:
+        a = []
+        a.append(PolutionAPI.getSensors())
         a.append(tmp)
         with open('air.json', 'w') as json_file:
             json.dump(a, json_file)
+    else:
+        a=b
 
-    with open('air.json','r') as json_file:
-        a = json.load(json_file)
-    return(a)
+
+    return(a[0])
 
 
 
